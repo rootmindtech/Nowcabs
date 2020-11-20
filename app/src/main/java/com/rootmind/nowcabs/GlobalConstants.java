add1fde9221eea775ac55ed92afbb9e668ae6907
saikirangandham <rootmindtech@gmail.com>
2020-11-20 21:05:01 +0400

    Commit on 20th Nov 2020, change in the host url

--- app/src/main/java/com/rootmind/nowcabs/GlobalConstants.java
+++ app/src/main/java/com/rootmind/nowcabs/GlobalConstants.java
@@ -16,7 +16,9 @@ public  final class GlobalConstants {
     //public static final String  HOST_URL = "https://nowcabs-dot-school-30cc4.appspot.com";
     //public static final String  HOST_URL = "https://nowcabs-dot-maabadi-219919.appspot.com/NowcabsWeb/Nowcabs";
     //public static final String  HOST_URL = "https://api.nowcabs.rootmindtech.com";
-    public static final String  HOST_URL = "https://emirateshost.rootmindtech.com/NowcabsWeb/Nowcabs"; //dubai house
+    //public static final String  HOST_URL = "https://emirateshost.rootmindtech.com/NowcabsWeb/Nowcabs"; //dubai house
+    //20-NOV-2020
+    public static final String  HOST_URL = "http://rootmind.ddns.net:8089/NowcabsWeb/Nowcabs"; //dubai house
     //public static final String  HOST_URL = "http://192.168.1.110:8084/NowcabsWeb/Nowcabs";
     public static final String SYSTEM_ERROR="Network unavailable, please try again";
 
